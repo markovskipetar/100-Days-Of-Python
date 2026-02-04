9b82260afbc64b4a9ee7f8d38c7b65d28d628289
Petar <petar.d.markovski@gmail.com>
2026-02-04 21:15:40 +0200

    Add day 2 task

---
+++ Day_2/syntax_fixing.py
@@ -0,0 +1,15 @@
+# Fix the code below 👇
+
+#print(Notes from Day 1")
+# print("The print statement is used to output strings")
+#print("Strings are strings of characters"
+#priint("String Concatenation is done with the + sign")
+#print(("New lines can be created with a \ and the letter n")
+
+
+print("Notes from Day 1") # missing opening quote
+print("The print statement is used to output strings") #wrong indentation
+print("Strings are strings of characters") # missing closing bracket
+print("String Concatenation is done with the" + "sign") # typo in print and wrong usage of '+' sign
+print("New lines can be created with a \ and the letter n") # extra opening bracket
+print("New lines\n")
